6d59653e9590fe2e5d7d5121a831d4179564fba4
Karol Appel <kappel@sii.pl>
2022-12-04 09:57:29 +0100

    day 4 2022 task: simplify code - namedtuple and better use of set functions

--- advent_of_code/aoc_2022/aoc_2022_04/aoc_2022_04.py
+++ advent_of_code/aoc_2022/aoc_2022_04/aoc_2022_04.py
@@ -1,4 +1,7 @@
 from pathlib import Path
+from collections import namedtuple
+
+Elves = namedtuple("Elves", "elf1 elf2")
 
 def parse(raw_input):
     parsed_input = []
@@ -6,33 +9,28 @@ def parse(raw_input):
         elf_ranges = []
         for elf in line.split(","):
             elf_range = elf.split("-")
-            elf_ranges.append(range(int(elf_range[0]), int(elf_range[-1]) + 1))
-        parsed_input.append(elf_ranges)
+            elf_ranges.append(set(range(int(elf_range[0]), int(elf_range[-1]) + 1)))
+        elves = Elves(elf_ranges[0], elf_ranges[1])
+        parsed_input.append(elves)
 
     return parsed_input
 
 
 def task1(parsed_input):
     fully_contain_couter = 0
-    for i, elf_pair in enumerate(parsed_input):
-        elf1 = set(elf_pair[0])
-        intersection = list(elf1.intersection(elf_pair[1]))
-        if not len(intersection):
-            continue
-        intersection_range = range(min(intersection), max(intersection) + 1)
-        if intersection_range in elf_pair:
+    for elf_pair in parsed_input:
+        if elf_pair.elf1.issuperset(elf_pair.elf2) or elf_pair.elf1.issubset(elf_pair.elf2):
             fully_contain_couter += 1
     return fully_contain_couter
 
 
 def task2(parsed_input):
-    overlap_couter = 0
-    for i, elf_pair in enumerate(parsed_input):
-        elf1 = set(elf_pair[0])
-        intersection = elf1.intersection(elf_pair[1])
-        if len(intersection):
-            overlap_couter += 1
-    return overlap_couter
+    overlap_counter = 0
+    for elf_pair in parsed_input:
+        is_disjoint = elf_pair.elf1.isdisjoint(elf_pair.elf2)
+        if not is_disjoint:
+            overlap_counter += 1
+    return overlap_counter
 
 
 if __name__ == "__main__":
